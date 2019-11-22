f5fbc0e97280b3676aff8bb4d8c227c036f949f0
Ju-Yeon Park <pjuyeon25@gmail.com>
2019-11-23 00:54:18 +0900

    Level 2

--- SOPT_SERVER/week2/projectGroup/routes/api/group.js
+++ SOPT_SERVER/week2/projectGroup/routes/api/group.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var read = require('../../module/csv2json.js');
 var router = express.Router();
-var filePath = "/Users/ju-yeon/Desktop/soptServer/week2/projectGroup/public/csvs/member.csv";
-
+var member = "/Users/ju-yeon/Desktop/soptServer/week2/projectGroup/public/csvs/member.csv";
+var group = "/Users/ju-yeon/Desktop/soptServer/week2/projectGroup/public/csvs/group.csv";
 
 /* GET users listing. */
 
 router.get('/', async function(req, res) {
     try{
-        const groupArray = await read(filePath);
+        const groupObject = await read(group);
+        const memberObject = await read(member);
+
+        var groupArray = [];
+        groupObject.forEach(element => {
+            groupArray.push(`${element.name}`);
+        });
+
 
         var nameAndGroup = ""; 
-        groupArray.forEach(element => {
-        nameAndGroup += `<br> ${element.name} : ${element.groupIdx}조`;
+        memberObject.forEach(element => {
+        nameAndGroup += `<br> ${element.name} : ${groupArray[element.groupIdx-1]}`;
     });
         res.status(200).send(nameAndGroup);
     }catch(err){
